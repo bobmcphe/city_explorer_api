5615efe1cf1febf1991fa99a33fa4fb239602e00
bobby@fivemoretalents.com <bobby@fivemoretalents.com>
2020-06-20 20:35:31 -0700

    not working yet

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const superagent = require("superagent");
 
 const PORT = process.env.PORT;
 
@@ -12,19 +12,26 @@ const app = express();
 app.use( cors() );
 
 
-app.get('/location', (request, response) => {
-  let data = require('./data/location.json');
-  let actualData = new Location(data[0]);
-  actualData.search_query = request.query.city;
-  response.status(200).json(actualData);
-});
+app.get("/location", (request, response) => {
+  const API1 = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${request.query.city}&format=json`;
 
-function Location( obj ) {
-  this.formatted_query = obj.display_name;
+
+superagent
+  .get(API1)
+  .then((data) => {
+    let locationObj = new Location(data.body[0], request.query.city);
+    response.status(200).send(locationObj);
+  })
+  .catch(() => {
+    response.status(500).send(console.log("You broke me, now fix it."));
+  });
+
+  function Location(obj, city) {
   this.latitude = obj.lat;
   this.longitude = obj.lon;
-
+  this.formatted_query = obj.display_name;
 }
+});
 
 //$('button').on('click', () => {})
 // app.get('/restaurants', (request, response) => 
@@ -46,25 +53,53 @@ function Location( obj ) {
 // }
 
 //////////////////////////WEATHER////////////////
+const API2 = `https://api.weatherbit.io/v2.0/forecast/daily?city=Seattle,WA&key=${process.env.WEATHER_API_KEY}`;
+console.log('weatherInfo: ', weatherInfo);
 
-app.get('/weather', (request, response) => {
-  let weatherData = require('./data/weather.json');
+superagent
+.get(API2)
+.then((results) => { 
+
+let weatherData = require('./data/weather.json');
   console.log('this is my weather data =============', weatherData);
-    const results = weatherData.data.map(stuff => {
+    let weatherInfo = weatherData.data.map(stuff => {
    
    return new Forecast(stuff);
   });
-  response.status(200).json(results);
-});
-// TODO SOEMTHIGN
+  response.status(200).json(weatherInfo);
 
 function Forecast(obj) {
   this.forecast = obj.weather.description;
   this.time = obj.datetime;
 
 };
+});
+////////////////////////////////////////////////
+////////////////////version two/////////////////
+///////////////////////////////////////////////
+// app.get('/weather', (request, response) => {
+//   let weatherData = require('./data/weather.json');
+//   console.log('this is my weather data =============', weatherData);
+//     const results = weatherData.data.map(stuff => {
+   
+//    return new Forecast(stuff);
+//   });
+//   response.status(200).json(results);
+// });
+// // TODO SOEMTHIGN
+
+// function Forecast(obj) {
+//   this.forecast = obj.weather.description;
+//   this.time = obj.datetime;
+
+// };
 
 
+
+
+////////////////////////////////////////////////
+//////////////////version one///////////////////////
+//////////////////////////////////////////////////
 // app.get('/weather', (request, response) => {
 //   let weatherData = require('./data/weather.json');
 //   let weeklyForecast = [];
