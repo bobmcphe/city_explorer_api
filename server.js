0d09928884c3b642aba8a8da79917f5da5688922
bobby@fivemoretalents.com <bobby@fivemoretalents.com>
2020-06-23 22:28:10 -0700

    nodemon freaking out

--- server.js
+++ server.js
@@ -24,14 +24,17 @@ function handleHomePage(request, response) {
 }
 
 // In Memory Cache
-let locations = {};
+let weathers
+ = {};
 
 
 function handleLocation(request, response) {
 
-  if (locations[request.query.city]) {
+  if (weathers
+    [request.query.city]) {
     console.log('we have it already...')
-    response.status(200).send(locations[request.query.city]);
+    response.status(200).send(weathers
+      [request.query.city]);
   }
   else {
     console.log('going to get it');
@@ -71,6 +74,84 @@ function Location(obj, city) {
 // function handleWeather(request, response) {
   
 // }
+
+
+///////////////////weather/////////////////////////////
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+let weathers = {};
+
+
+function handleLocation(request, response) {
+
+  if (weathers
+    [request.query.city]) {
+    console.log('we have it already...')
+    response.status(200).send(weathers
+      [request.query.city]);
+  }
+  else {
+    console.log('going to get it');
+    fetchLocationDataFromAPI(request.query.city, response);
+  }
+
+}
+
+function fetchLocationDataFromAPI(city, response) {
+
+  const API1 = 'https://us1.locationiq.com/v1/search.php';
+
+  let queryObject = {
+    key: process.env.GEOCODE_API_KEY,
+    q: city,
+    format: 'json'
+  }
+
+  superagent
+  .get(API1)
+  .then((data) => {
+    let locationObj = new Location(data.body[0], request.query.city);
+    response.status(200).send(locationObj);
+  })
+  .catch(() => {
+    response.status(500).send(console.log("You broke me, now fix it."));
+  });
+}
+
+function Location(obj, city) {
+  this.latitude = obj.lat;
+  this.longitude = obj.lon;
+  this.formatted_query = obj.display_name;
+  this.search_query = city;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.use('*', (request,response) => {
   response.status(404).send('Huhhhh?');
