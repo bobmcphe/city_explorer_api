acf62cf79b426667e7eb8a25a13687603066f909
bobby@fivemoretalents.com <bobby@fivemoretalents.com>
2020-06-25 19:41:50 -0700

    added a required section

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const { response, request} = require('express');
 
 const PORT = process.env.PORT;
 
